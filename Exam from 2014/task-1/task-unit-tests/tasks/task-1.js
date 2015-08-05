1f97d21342d28bc0d8cc0c0af7aab6e8ccb14883
Alex-Bubblemaster <shiatzu@gmail.com>
2015-08-05 19:39:38 +0300

    Correct view. Need to attach events and sort input

--- Exam from 2014/task-1/task-unit-tests/tasks/task-1.js
+++ Exam from 2014/task-1/task-unit-tests/tasks/task-1.js
@@ -1,25 +1,69 @@
-/* globals module */
-function solve(selector, items) {
- var container = document.querySelector(selector),
-     leftDiv = document.createElement('div'),
-     rightDiv = document.createElement('div'),
-     bigImage = document.createElement('img');
+function solve() {
+    return function (selector, items) {
 
-    //left div image preview
+        var container = document.querySelector(selector),
+            leftDiv = document.createElement('div'),
+            rightDiv = document.createElement('div'),
+            bigImage = document.createElement('img'),
+            bigTitle = document.createElement('h1'),
+            input = document.createElement('input');
 
-    leftDiv.className += ' image-preview';
-    bigImage.src = items[0].url;
-    leftDiv.appendChild(bigImage);
+        //left div image preview
+        leftDiv.className += ' image-preview';
+        leftDiv.style.float = 'left';
+        leftDiv.style.align = 'center';
+
+        bigImage.src = items[0].url;
+        bigImage.style.width = '400px';
+        bigImage.style.height = '400px';
+
+        bigTitle.innerHTML = items[0].title;
+        bigTitle.style.textAlign = 'center';
+
+        leftDiv.appendChild(bigTitle); //without the h1 did not pass the test!
+        leftDiv.appendChild(bigImage);
 
 
 
+        // right div
+
+        rightDiv.style.display = 'inline-block';
+        rightDiv.style.textAlign = 'center';
+        rightDiv.style.width = '250px';
+        rightDiv.style.height = '450px';
+        rightDiv.style.overflow = 'scroll';
 
 
 
+        rightDiv.appendChild(input);
+        //generate objects
+        var html = '';
+        for (var i = 0, itemsLen = items.length; i < itemsLen; i += 1) {
+            html += '<div class="image-container">';
+            html += '<h1>' + items[i].title + '</h1>';
+            html += '<img src="' + items[i].url + '" width="190">';
+            html += '</div>';
+        }
+        rightDiv.innerHTML += html;
 
+        container.appendChild(leftDiv);
+        container.appendChild(rightDiv);
 
-    container.appendChild(leftDiv);
-    container.appendChild(rightDiv);
+        var titles = document.getElementsByTagName('h1');
+        [].forEach.call(titles, function (title) {
+            title.style.textAlign = 'center';
+            title.style.fontSize = '18px';
+        });
+
+        var imageDivs = document.getElementsByClassName('image-container');
+        [].forEach.call(imageDivs, function (imageDiv) {
+            imageDiv.style.textAlign = 'center';
+        });
+
+        var inputField = document.getElementsByTagName('input');
+        inputField.style.position = 'absolute';
+
+    };
 }
 
 module.exports = solve;
